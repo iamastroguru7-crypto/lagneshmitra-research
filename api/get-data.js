fbfd92c844fd56e8ecd5081fd1b7ce7182f1d693
iamastroguru7-crypto <iamastroguru7@gmail.com>
2026-07-19 23:46:21 +0530

    Update get-data.js

--- api/get-data.js
+++ api/get-data.js
@@ -2,16 +2,13 @@ export default async function handler(req, res) {
     const { dob, tob, loc } = req.query;
     const apiKey = "sk_live_0QU8V8YdAMv7v08zj1oST2kqk7Mi9Oxf19f54OiF";
     
-    // Naya structure (URL Builder ke hisaab se)
-    // Format: .../Calculate/Planet/Sun/Location/[Loc]/Time/[Time]/[Date]/[Offset]
-    const url = `https://api.vedastro.org/Calculate/Planet/Sun/Location/${loc}/Time/${tob}/${dob}/+05:30`;
+    // Sahi URL path (bin /api ke)
+    const url = `https://api.vedastro.org/Calculate/PlanetName/Planet/Sun/Time/${tob}/${dob}/+00:00/Location/${loc}/UK`;
 
     try {
         const response = await fetch(url, {
             method: 'GET',
-            headers: {
-                "x-api-key": apiKey
-            }
+            headers: { "x-api-key": apiKey }
         });
         const data = await response.json();
         res.status(200).json(data);
