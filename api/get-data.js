5ec794fae90b132b08b608b211d28391dbd7db81
iamastroguru7-crypto <iamastroguru7@gmail.com>
2026-07-19 23:29:43 +0530

    Update get-data.js

--- api/get-data.js
+++ api/get-data.js
@@ -1,9 +1,6 @@
-// File: api/get-data.js
 export default async function handler(req, res) {
     const { dob, tob, loc } = req.query;
     const apiKey = "sk_live_0QU8V8YdAMv7v08zj1oST2kqk7Mi9Oxf19f54OiF";
-    
-    // VedAstro API URL
     const url = `https://api.vedastro.org/api/APIKey/${apiKey}/Calculate/PlanetName/Planet/Sun/Time/${tob}/${dob}/+00:00/Location/${loc}/UK`;
 
     try {
@@ -11,7 +8,6 @@ export default async function handler(req, res) {
         const data = await response.json();
         res.status(200).json(data);
     } catch (error) {
-        res.status(500).json({ error: "Failed to fetch from VedAstro" });
+        res.status(500).json({ error: error.message });
     }
 }
-
